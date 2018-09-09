238a08f7869a0b42742e271fb9bcb885c76fed09
Paulo Casaes <paulo.casaes@zwift.com>
2018-09-09 20:39:08 -0300

    [PC] Parsing Requests Paths

--- monitoring-uptime-app/index.js
+++ monitoring-uptime-app/index.js
@@ -5,11 +5,24 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 
 // The server should respond to all requests with a string.
 const server = http.createServer((req, res) => {
+    // Get the URL and parse it.
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl
+        .pathname
+        .replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello World!\n');
+
+    // Log the request
+    console.log(`Request received on path: ${path}`);
 });
 
 // Start the server, and have it listen to port 3000.
