df8e2bf5084eae80b775ede1f5802ce39a3e3716
Paulo Casaes <paulo.casaes@zwift.com>
2018-09-09 20:47:40 -0300

    [PC] Parsing HTTP Methods

--- monitoring-uptime-app/index.js
+++ monitoring-uptime-app/index.js
@@ -10,19 +10,27 @@ const url = require('url');
 
 // The server should respond to all requests with a string.
 const server = http.createServer((req, res) => {
+    // Get request time
+    const requestTime = new Date();
+
     // Get the URL and parse it.
     const parsedUrl = url.parse(req.url, true);
+
 
     // Get the path
     const path = parsedUrl
         .pathname
         .replace(/^\/+|\/+$/g, '');
 
+    // Get the HTTP Method
+    const method = req.method
+        .toUpperCase();
+
     // Send the response
     res.end('Hello World!\n');
 
     // Log the request
-    console.log(`Request received on path: ${path}`);
+    console.log(`${requestTime.toISOString()} Request received: ${method} ${path}`);
 });
 
 // Start the server, and have it listen to port 3000.
