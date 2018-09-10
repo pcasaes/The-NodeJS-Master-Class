3754f651b14896806d3b51a7197875900e241cac
Paulo Casaes <paulo.casaes@zwift.com>
2018-09-10 20:46:09 -0300

    [PC] Parsing Payloads

--- monitoring-uptime-app/index.js
+++ monitoring-uptime-app/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 
 // The server should respond to all requests with a string.
@@ -32,13 +33,27 @@ const server = http.createServer((req, res) => {
     // Get the headers as an object
     const headers = req.headers;
 
-    // Send the response
-    res.end('Hello World!\n');
+    // Get the payload, if any
+    const decoder = new StringDecoder("utf-8");
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
 
-    // Log the request
-    console.log(`${requestTime.toISOString()} Request received: ${method} ${path}
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello World!\n');
+
+        // Log the request
+        console.log(`${requestTime.toISOString()} Request received: ${method} ${path}
     Query string parameters:`, queryStringObject);
-    console.log('    Headers:', headers)
+        console.log('    Headers:', headers);
+        console.log('    Payload:', buffer);
+
+    });
+
 });
 
 // Start the server, and have it listen to port 3000.
