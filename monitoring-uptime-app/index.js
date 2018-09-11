0999c18fccdd7fe554ac651a2e4f70974fb7e17d
Paulo Casaes <paulo.casaes@zwift.com>
2018-09-11 11:04:30 -0300

    [PC] Routing Requests

--- monitoring-uptime-app/index.js
+++ monitoring-uptime-app/index.js
@@ -43,14 +43,50 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
         buffer += decoder.end();
 
-        // Send the response
-        res.end('Hello World!\n');
+        // Choose the handler this request should go to. If one is not found, use the not found handler
+        const chosenHandler = typeof(router[path]) !== 'undefined' ? router[path] : handlers.notFound;
 
-        // Log the request
-        console.log(`${requestTime.toISOString()} Request received: ${method} ${path}
-    Query string parameters:`, queryStringObject);
-        console.log('    Headers:', headers);
-        console.log('    Payload:', buffer);
+        // Construct the data object to send to the handler
+        const data = {
+            'path': path,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        // Router the request to the handler specified in the router
+        chosenHandler(data, (err, statusCode, responsePayload) => {
+            res.setHeader('content-type', 'application/json');
+            if (err) {
+                res.writeHead(500);
+                res.end(err.message);
+
+                // Log the request
+                console.error(`${requestTime.toISOString()} Request received: ${data.method} ${data.path}
+    Error:`, err);
+
+            } else {
+                // Use the status code called back by the handler, or default to 200
+                statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+                // Use the payload called back by the handler. or default to an empty object
+                responsePayload = typeof(responsePayload) === 'object' ? responsePayload : {};
+
+                // Convert the payload to a string
+                const payloadString = JSON.stringify(responsePayload);
+
+                // Return the response
+                res.writeHead(statusCode);
+                res.end(payloadString);
+
+                // Log the request
+                console.log(`${requestTime.toISOString()} Request received: ${data.method} ${data.path}; Response: ${statusCode}, Payload:`, payloadString);
+
+            }
+
+        });
+
 
     });
 
@@ -60,3 +96,38 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('The server is listening on port 3000 now.');
 });
+
+// Define the handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+    // Callback an http status code, and a payload object
+
+    callback(null, 406, {'name': 'sample handler'});
+};
+
+// Ping handler
+handlers.echo = (data, callback) => {
+    // Callback an http status code, and a payload object
+    if (data.method === 'POST') {
+        if (data.headers['content-type'] === 'application/json') {
+            callback(null, 200, JSON.parse(data.payload));
+        } else {
+            callback(null, 200, {'payload': data.payload});
+        }
+    } else {
+        callback(null, 406, {'error': 'only POST is supported'});
+    }
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+    callback(null, 404);
+};
+
+// Define a request router
+const router = {
+    'sample': handlers.sample,
+    'echo': handlers.echo,
+};
