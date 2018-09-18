2c67de819968793a7392dc2a372818fe03e21dce
Paulo Casaes <paulo.casaes@zwift.com>
2018-09-18 15:25:54 -0300

    [PC] Adding Configuration

--- monitoring-uptime-app/index.js
+++ monitoring-uptime-app/index.js
@@ -7,6 +7,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 
 // The server should respond to all requests with a string.
@@ -92,9 +93,9 @@ const server = http.createServer((req, res) => {
 
 });
 
-// Start the server, and have it listen to port 3000.
-server.listen(3000, () => {
-    console.log('The server is listening on port 3000 now.');
+// Start the server, and have it listen to port ${config.port}.
+server.listen(config.port, () => {
+    console.log(`The server is listening on port ${config.port} in ${config.envName} mode.`);
 });
 
 // Define the handlers
